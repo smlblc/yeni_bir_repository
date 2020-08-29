4573a1d5e9b6e8efa4b66da96ad230f51dc99f3f
smlblc <samilbalci.us@gmail.com>
2020-08-29 16:19:13 -0400

    Ornek3 class i eklendi

---
+++ src/Klasor/Ornek3.java
@@ -0,0 +1,7 @@
+package Klasor;
+
+public class Ornek3 {
+    public static void main(String[] args) {
+        System.out.println("Ornek 3 son haliniolusturmadigimiz bir kod oldugu icin once brach`e yukleyelim");
+    }
+}
