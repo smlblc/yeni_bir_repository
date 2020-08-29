223ca5a5bb1e50a4171310144afea599c168af60
smlblc <samilbalci.us@gmail.com>
2020-08-29 15:50:31 -0400

    Ornek1 class i eklendi

---
+++ src/Klasor/Ornek.java
@@ -0,0 +1,8 @@
+package Klasor;
+
+public class Ornek {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba github");
+    }
+}
---
+++ src/Klasor/Ornek1.java
@@ -0,0 +1,8 @@
+package Klasor;
+
+public class Ornek1 {
+
+    public static void main(String[] args) {
+        System.out.println("Ornek_1");
+    }
+}
